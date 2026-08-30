2a24df24c2a8998e7c0b6b226ec215bd0033ab96
Aman Yadav <aman.ku.yadav2001@gmail.com>
2026-08-30 12:52:59 +0530

    Clean file structure

--- debug.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, 'c:\\Users\\ASUS\\OneDrive\\Desktop\\edgeDash')
-
-from edgedash.config import load_config
-from edgedash.orchestrator import run_cycle
-
-config = load_config()
-run_cycle(config)
--- run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\ASUS\OneDrive\Desktop\edgeDash"
-python run_cycle.py
-pause
